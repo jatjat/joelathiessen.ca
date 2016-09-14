d575e82038a2eaae43f089616d34207344545a88
Joel Thiessen <jatjat@users.noreply.github.com>
2016-09-14 17:14:46 -0500

    Show a generic React-Leaflet map

--- js/robotsAppClient.js
+++ js/robotsAppClient.js
@@ -1,5 +1,8 @@
 import React from 'react';
-var Io = require('socket.io-client');
+import { Panel } from 'react-bootstrap';
+import IO from 'socket.io-client';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 export class RobotsApp extends React.Component {
   constructor(props) {
@@ -9,7 +12,7 @@ export class RobotsApp extends React.Component {
     }
   }
   componentDidMount() {
-    this.io = Io(this.props.namespace, {
+    this.io = IO(this.props.namespace, {
       reconnect: true,
       'connect timeout': 5000
     });
@@ -42,9 +45,16 @@ export class RobotsApp extends React.Component {
   }
 
   render() {
+    const position = [51.505, -0.09];
+
     return (
       <div>RobotsApp!
           {this.state.connected}
+          <Panel className="mapPanel">
+              <Map center={position} zoom={13}>
+                  <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
+              </Map>
+          </Panel>
       </div>
       );
   }
