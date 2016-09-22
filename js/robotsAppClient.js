a6e1885df0556d8a3ea81f691d1b4c11071286e8
Joel Thiessen <jatjat@users.noreply.github.com>
2016-09-22 12:48:02 -0500

    Add Help modal

--- js/robotsAppClient.js
+++ js/robotsAppClient.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, ButtonGroup, ButtonToolbar, ControlLabel, Glyphicon, Grid, FormGroup, FormControl, HelpBlock, Row, Col, Panel } from 'react-bootstrap';
+import { Button, ButtonGroup, ButtonToolbar, ControlLabel, Glyphicon, Grid, FormGroup, FormControl, HelpBlock, Modal, Row, Col, Panel } from 'react-bootstrap';
 import Validator from 'validator';
 import IO from 'socket.io-client';
 import Leaflet from 'leaflet';
@@ -12,15 +12,17 @@ export class RobotsApp extends React.Component {
       connected: "uninitialized",
       numParticles: "",
       sensorDistStdev: "",
-      sensorAngStdev: ""
+      sensorAngStdev: "",
+      showHelpModal: false
     }
     this.io = IO(this.props.namespace, {
       reconnect: true,
-      'connect timeout': 5000
     });
     this.handleNumParticlesChange = this.handleNumParticlesChange.bind(this);
     this.handleSensorDistStdevChange = this.handleSensorDistStdevChange.bind(this);
     this.handleSensorAngStdevChange = this.handleSensorAngStdevChange.bind(this);
+    this.openHelpModal = this.openHelpModal.bind(this);
+    this.closeHelpModal = this.closeHelpModal.bind(this);
   }
 
   componentDidMount() {
@@ -50,6 +52,18 @@ export class RobotsApp extends React.Component {
 
   componentWillUnmount() {
     this.io.disconnect();
+  }
+
+  openHelpModal() {
+    this.setState({
+      showHelpModal: true
+    });
+  }
+
+  closeHelpModal() {
+    this.setState({
+      showHelpModal: false
+    });
   }
 
   getNumParticlesValidationState(includeMsg) {
@@ -181,65 +195,91 @@ export class RobotsApp extends React.Component {
       );
     }
     return (
-      <Grid>
-          <Row>
-              <Col xs={12} md={8} mdPush={4}>
-              <Panel className="mapPanel">
-                  <RMap io={this.io} connected={this.state.connected} />
-              </Panel>
-              </Col>
-              <Col xs={12} md={4} mdPull={8}>
-              <Panel header="FastSLAM Settings">
-                  <form>
-                      <FormGroup controlId="numParticlesForm" validationState={this.getNumParticlesValidationState()}>
-                          <ControlLabel>Number of Particles</ControlLabel>
-                          <FormControl type="text" value={this.state.numParticles} placeholder="Number of particles (1-100, default: 10)" onChange={this.handleNumParticlesChange} />
-                          <FormControl.Feedback />
-                          {numParticlesHelpBlock}
-                      </FormGroup>
-                      <FormGroup controlId="sensorDistStdevForm" validationState={this.getSensorDistStdevValidationState()}>
-                          <ControlLabel>Hypothetical sensor distance standard deviation</ControlLabel>
-                          <FormControl type="text" value={this.state.sensorDistStdev} placeholder="Hypothetical σ (default: 2 units)" onChange={this.handleSensorDistStdevChange} />
-                          <FormControl.Feedback />
-                          {sensorDistStdevHelpBlock}
-                      </FormGroup>
-                      <FormGroup controlId="sensorAngStdevForm" validationState={this.getSensorAngStdevValidationState()}>
-                          <ControlLabel>Hypothetical sensor angle standard deviation</ControlLabel>
-                          <FormControl type="text" value={this.state.sensorAngStdev} placeholder="Hypothetical σ (default: 1 radian)" onChange={this.handleSensorAngStdevChange} />
-                          <FormControl.Feedback />
-                          {sensorAngStdevHelpBlock}
-                      </FormGroup>
-                      <ButtonToolbar justified>
+      <div>
+          <Modal show={this.state.showHelpModal} onHide={this.closeHelpModal}>
+              <Modal.Header closeButton>
+                  <Modal.Title>What do these settings do? And what is FastSLAM?</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                  <h5>What is FastSLAM?</h5>
+                  <p>
+                      FastSLAM is an algorithm that lets an agent like a robot keep an accurate map with information about where it has been, and what it has seen, using odometric and laser or video sensor data.
+                  </p>
+                  <p>
+                      A robot implementing FastSLAM has particles each representing a possible robot "pose" (position and orientation). For a given step of the algorithm, the robot combines odometry and measurements to all the "features" that its sensors can see, as if from each of its particles. If a feature has been seen before in the position that a particle expects, (i.e. it corresponds to a "landmark"), the "weight" of the particle is adjusted based on how good the match is. Otherwise the feature is added as a new landmark. Finally, the particles are replaced with copies of the highest weighted particles. You can read more <a href="http://robots.stanford.edu/papers/montemerlo.fastslam-tr.pdf">here.</a>
+                  </p>
+                  <h5>Number of Particles:</h5> FastSLAM is O(mlog(n)) where m is the number of particles, and n the number of landmarks. While more particles cause better accuracy, in aggregate they are expensive, and you may get excellent results even with 5-10 particles!
+                  <p>
+                  </p>
+                  <h5>Hypothetical sensor distance & angle standard deviations:</h5>
+                  <p>
+                      FastSLAM guesses the accuracy of its sensors. The real accuracies of this robot's sensor are σ = 0.1 distance units and σ = 0.1 radians. Tuning these may produce better results.
+                  </p>
+              </Modal.Body>
+              <Modal.Footer>
+                  <Button onClick={this.closeHelpModal}>Close</Button>
+              </Modal.Footer>
+          </Modal>
+          <Grid>
+              <Row>
+                  <Col xs={12} md={8} mdPush={4}>
+                  <Panel className="mapPanel">
+                      <RMap io={this.io} connected={this.state.connected} />
+                  </Panel>
+                  </Col>
+                  <Col xs={12} md={4} mdPull={8}>
+                  <Panel header="FastSLAM Settings">
+                      <form>
+                          <FormGroup controlId="numParticlesForm" validationState={this.getNumParticlesValidationState()}>
+                              <ControlLabel>Number of Particles</ControlLabel>
+                              <FormControl type="text" value={this.state.numParticles} placeholder="Number of particles (1-100, default: 10)" onChange={this.handleNumParticlesChange} />
+                              <FormControl.Feedback />
+                              {numParticlesHelpBlock}
+                          </FormGroup>
+                          <FormGroup controlId="sensorDistStdevForm" validationState={this.getSensorDistStdevValidationState()}>
+                              <ControlLabel>Hypothetical sensor distance standard deviation</ControlLabel>
+                              <FormControl type="text" value={this.state.sensorDistStdev} placeholder="Hypothetical σ (default: 0.1 units)" onChange={this.handleSensorDistStdevChange} />
+                              <FormControl.Feedback />
+                              {sensorDistStdevHelpBlock}
+                          </FormGroup>
+                          <FormGroup controlId="sensorAngStdevForm" validationState={this.getSensorAngStdevValidationState()}>
+                              <ControlLabel>Hypothetical sensor angle standard deviation</ControlLabel>
+                              <FormControl type="text" value={this.state.sensorAngStdev} placeholder="Hypothetical σ (default: 0.1 radians)" onChange={this.handleSensorAngStdevChange} />
+                              <FormControl.Feedback />
+                              {sensorAngStdevHelpBlock}
+                          </FormGroup>
+                          <ButtonToolbar justified>
+                              <ButtonGroup>
+                                  <Button bsStyle="info" onClick={this.openHelpModal}>
+                                      <Glyphicon glyph="question-sign" /> What do these settings do?
+                                  </Button>
+                              </ButtonGroup>
+                              <ButtonGroup>
+                                  <Button bsStyle="primary">
+                                      <Glyphicon glyph="ok" /> Apply
+                                  </Button>
+                              </ButtonGroup>
+                          </ButtonToolbar>
+                      </form>
+                  </Panel>
+                  <Panel header="Simulation Settings">
+                      <ButtonToolbar>
                           <ButtonGroup>
-                              <Button type="submit" bsStyle="info">
-                                  <Glyphicon glyph="question-sign" /> What do these settings do?
+                              <Button bsStyle="danger">
+                                  <Glyphicon glyph="remove-circle" /> Reset Simulation
                               </Button>
                           </ButtonGroup>
                           <ButtonGroup>
-                              <Button type="submit" bsStyle="primary">
-                                  <Glyphicon glyph="ok" /> Apply
+                              <Button bsStyle="success">
+                                  <Glyphicon glyph="play" /> Start
                               </Button>
                           </ButtonGroup>
                       </ButtonToolbar>
-                  </form>
-              </Panel>
-              <Panel header="Simulation Settings">
-                  <ButtonToolbar>
-                      <ButtonGroup>
-                          <Button bsStyle="danger">
-                              <Glyphicon glyph="remove-circle" /> Reset Simulation
-                          </Button>
-                      </ButtonGroup>
-                      <ButtonGroup>
-                          <Button bsStyle="success">
-                              <Glyphicon glyph="play" /> Start
-                          </Button>
-                      </ButtonGroup>
-                  </ButtonToolbar>
-              </Panel>
-              </Col>
-          </Row>
-      </Grid>
+                  </Panel>
+                  </Col>
+              </Row>
+          </Grid>
+      </div>
       );
   }
 }
