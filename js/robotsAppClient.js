0469353ebdcbdbdd7a6bb2dd4305f900803f382d
Joel Thiessen <jatjat@users.noreply.github.com>
2016-09-21 21:58:42 -0500

    Validate sensor's hypothetical distance and angle stdev

--- js/robotsAppClient.js
+++ js/robotsAppClient.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ControlLabel, Grid, FormGroup, FormControl, HelpBlock, Row, Col, Panel } from 'react-bootstrap';
+import { Button, ButtonGroup, ButtonToolbar, ControlLabel, Glyphicon, Grid, FormGroup, FormControl, HelpBlock, Row, Col, Panel } from 'react-bootstrap';
 import Validator from 'validator';
 import IO from 'socket.io-client';
 import Leaflet from 'leaflet';
@@ -10,13 +10,17 @@ export class RobotsApp extends React.Component {
     super(props)
     this.state = {
       connected: "uninitialized",
-      numParticles: ""
+      numParticles: "",
+      sensorDistStdev: "",
+      sensorAngStdev: ""
     }
     this.io = IO(this.props.namespace, {
       reconnect: true,
       'connect timeout': 5000
     });
-    this.handleNumParticlesChange = this.handleNumParticlesChange.bind(this)
+    this.handleNumParticlesChange = this.handleNumParticlesChange.bind(this);
+    this.handleSensorDistStdevChange = this.handleSensorDistStdevChange.bind(this);
+    this.handleSensorAngStdevChange = this.handleSensorAngStdevChange.bind(this);
   }
   componentDidMount() {
 
@@ -52,8 +56,8 @@ export class RobotsApp extends React.Component {
 
   getNumParticlesValidationState(includeMsg) {
 
-    var validity;
-    var msg;
+    var validity = null;
+    var msg = null;
     if (Validator.isInt(this.state.numParticles, {
         min: 1,
         max: 50
@@ -64,22 +68,23 @@ export class RobotsApp extends React.Component {
         max: 100
       })) {
       validity = "warning";
-      msg = "A large number of particles may result in diminished localisation returns";
+      msg = "A large number of particles may result in diminished localization returns";
     } else if (Validator.isInt(this.state.numParticles)) {
       validity = "error";
       msg = "An integer between 1 and 100 is required";
     } else if (this.state.numParticles != "") {
       validity = "error";
       msg = "A valid integer is required";
-    } else {
-      validity = null;
     }
 
-      if(includeMsg == true) {
-        return { validity: validity, msg : msg };
-      } else {
-        return validity;
-      }
+    if (includeMsg == true) {
+      return {
+        validity: validity,
+        msg: msg
+      };
+    } else {
+      return validity;
+    }
   }
 
   handleNumParticlesChange(e) {
@@ -88,13 +93,94 @@ export class RobotsApp extends React.Component {
     });
   }
 
-  render() {
-    var helpBlock = null;
-    var numPartValidity = this.getNumParticlesValidationState(true);
+  getSensorDistStdevValidationState(includeMsg) {
 
+    var validity = null;
+    var msg = null;
+    if (Validator.isFloat(this.state.sensorDistStdev, {
+        min: 0
+      })) {
+      validity = "success";
+    } else if (Validator.isFloat(this.state.sensorDistStdev)) {
+      validity = "error";
+      msg = "A positive number is required";
+    } else if (this.state.sensorDistStdev != "") {
+      validity = "error";
+      msg = "A valid postive number is required";
+    }
+
+    if (includeMsg == true) {
+      return {
+        validity: validity,
+        msg: msg
+      };
+    } else {
+      return validity;
+    }
+  }
+
+  handleSensorDistStdevChange(e) {
+    this.setState({
+      sensorDistStdev: e.target.value
+    });
+  }
+
+  getSensorAngStdevValidationState(includeMsg) {
+
+    var validity = null;
+    var msg = null;
+    if (Validator.isFloat(this.state.sensorAngStdev, {
+        min: 0
+      })) {
+      validity = "success";
+    } else if (Validator.isFloat(this.state.sensorAngStdev)) {
+      validity = "error";
+      msg = "A positive number is required";
+    } else if (this.state.sensorAngStdev != "") {
+      validity = "error";
+      msg = "A valid positive number is required";
+    }
+
+    if (includeMsg == true) {
+      return {
+        validity: validity,
+        msg: msg
+      };
+    } else {
+      return validity;
+    }
+  }
+
+  handleSensorAngStdevChange(e) {
+    this.setState({
+      sensorAngStdev: e.target.value
+    });
+  }
+
+  render() {
+    var numParticlesHelpBlock = null;
+    var numPartValidity = this.getNumParticlesValidationState(true);
     if (numPartValidity.validity != 'success') {
-      helpBlock = (<HelpBlock>{numPartValidity.msg}</HelpBlock>
-      )
+      numParticlesHelpBlock = (<HelpBlock>
+                                   {numPartValidity.msg}
+                               </HelpBlock>
+      );
+    }
+    var sensorDistStdevHelpBlock = null;
+    var sensorDistStdevValidity = this.getSensorDistStdevValidationState(true);
+    if (sensorDistStdevValidity.validity != 'success') {
+      sensorDistStdevHelpBlock = (<HelpBlock>
+                                      {sensorDistStdevValidity.msg}
+                                  </HelpBlock>
+      );
+    }
+    var sensorAngStdevHelpBlock = null;
+    var sensorAngStdevValidity = this.getSensorAngStdevValidationState(true);
+    if (sensorAngStdevValidity.validity != 'success') {
+      sensorAngStdevHelpBlock = (<HelpBlock>
+                                     {sensorAngStdevValidity.msg}
+                                 </HelpBlock>
+      );
     }
     return (
       <Grid>
@@ -105,13 +191,25 @@ export class RobotsApp extends React.Component {
               </Panel>
               </Col>
               <Col xs={12} md={4} mdPull={8}>
-              <Panel>
+              <Panel header="FastSLAM Settings">
                   <form>
                       <FormGroup controlId="numParticlesForm" validationState={this.getNumParticlesValidationState()}>
                           <ControlLabel>Number of Particles</ControlLabel>
-                          <FormControl type="text" value={this.state.numParticles} placeholder="Number of particles (1-100)" onChange={this.handleNumParticlesChange} />
+                          <FormControl type="text" value={this.state.numParticles} placeholder="Number of particles (1-100, default: 10)" onChange={this.handleNumParticlesChange} />
                           <FormControl.Feedback />
-                          {helpBlock}
+                          {numParticlesHelpBlock}
+                      </FormGroup>
+                      <FormGroup controlId="sensorDistStdevForm" validationState={this.getSensorDistStdevValidationState()}>
+                          <ControlLabel>Hypothetical sensor distance standard deviation</ControlLabel>
+                          <FormControl type="text" value={this.state.sensorDistStdev} placeholder="Hypothetical σ (default: 2 units)" onChange={this.handleSensorDistStdevChange} />
+                          <FormControl.Feedback />
+                          {sensorDistStdevHelpBlock}
+                      </FormGroup>
+                      <FormGroup controlId="sensorAngStdevForm" validationState={this.getSensorAngStdevValidationState()}>
+                          <ControlLabel>Hypothetical sensor angle standard deviation</ControlLabel>
+                          <FormControl type="text" value={this.state.sensorAngStdev} placeholder="Hypothetical σ (default: 1 radian)" onChange={this.handleSensorAngStdevChange} />
+                          <FormControl.Feedback />
+                          {sensorAngStdevHelpBlock}
                       </FormGroup>
                   </form>
               </Panel>
