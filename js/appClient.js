43f636204986842f46c67bcf20103fd9428946f9
Joel Thiessen <jatjat@users.noreply.github.com>
2017-02-10 16:10:41 -0600

    Move About to its own file

--- js/appClient.js
+++ js/appClient.js
@@ -6,6 +6,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { ReactPDF } from './pdfViewer';
 import { RobotsApp } from './RobotsAppClient/robotsAppClient';
 import { rotl33tStr } from './util'
+import { About } from './about'
 
 class App extends React.Component {
   render() {
@@ -140,32 +141,6 @@ class Resume extends React.Component {
 class Robots extends React.Component {
   render() {
     return (<RobotsApp namespace="/" />);
-  }
-}
-
-class About extends React.Component {
-  render() {
-    return (
-      <Grid>
-          <Row>
-              <Col xs={12} md={9}>
-              <Panel>
-                  <Col xs={4} md={4}>
-                  <Image src="/img/JoelThiessenPhotoSmall.jpeg" rounded responsive />
-                  </Col>
-                  <span></span>
-                  <Col xs={4} md={8}>
-                  <Panel>
-                      About this website: it's mostly for projects I think are cool, and my resume.
-                      <br />
-                      <br /> About me: I enjoy reading, robotics, and creating things.
-                  </Panel>
-                  </Col>
-              </Panel>
-              </Col>
-          </Row>
-      </Grid>
-      );
   }
 }
 
