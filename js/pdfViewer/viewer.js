1e6156b01182cd0881a31b811b667a2824bfbfc7
Joel Thiessen <jatjat@users.noreply.github.com>
2017-02-28 19:22:21 -0600

    add viewer.js

---
+++ js/pdfViewer/viewer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Page, PDF } from './reactPDF.js'
+
+export class Viewer extends React.Component {
+  static contextTypes = PDF.childContextTypes
+
+  render() {
+    let {pdf} = this.context
+    let numPages = pdf ? pdf.pdfInfo.numPages : 0
+    let fingerprint = pdf ? pdf.pdfInfo.fingerprint : 'none'
+    let pages = Array.apply(null, {
+      length: numPages
+    })
+      .map((v, i) => (<Page index={i + 1} key={`${fingerprint}-${i}`} />))
+
+    return (
+      <div className='pdf-viewer'>
+          {pages}
+      </div>
+    )
+  }
+}
