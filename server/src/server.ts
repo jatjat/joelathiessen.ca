e3476e84935a80d54a4a0f460841642c1bf80d90
Joel Thiessen <jatjat@users.noreply.github.com>
2018-08-19 00:15:48 -0500

    Hardcode kaly2 port for now

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ import IO from "socket.io";
 const OUTGOING_FRACTION_DIGITS = 2; // limit length of number strings sent from kaly2 to client
 const DEFAULT_WS_ADDR = "ws://localhost:9000/api/ws/robot";
 const KALY2_SERVICE_WS_ADDR = kaly2ServiceName =>
-  `ws://${kaly2ServiceName}/api/ws/robot`;
+  `ws://${kaly2ServiceName}:9000/api/ws/robot`;
 const KALY_PING_INTERVAL_MS = 15000;
 const DEFAULT_PORT = 3000;
 const MAX_ALLOWED_PARTICLES = 100;
